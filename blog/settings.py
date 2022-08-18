60a550978d0e92fc40b9177b085b8b136676e282
Mateokruljac <mateokruljac23@gmail.com>
2022-08-18 14:52:24 +0200

    Default user image

--- blog/settings.py
+++ blog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os 
 import os
 from pathlib import Path
 
@@ -116,22 +116,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR,'static')
-]
 #postaviti statične i media url
 STATIC_URL = '/static/'
 MEDIA_URL  = '/media/' 
 #media root
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-
-STATICFILES_DIR = (
-    os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
 )
 
-STATIC_ROOT = os.path.join(
-    BASE_DIR,"static"
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
